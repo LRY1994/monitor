657a5b0fe4a9ff1cee70277f09df1fe0c8909c8a
LRY1994 <949892801@qq.com>
2016-06-01 10:33:06 +0800

    饼图更新，加了首页

--- js/app.js
+++ js/app.js
@@ -3,7 +3,6 @@
  */
 var pageSize=15;
 var num;
-
 var onNum=0;
 var offNum=0;
 var datalist =new Array();
@@ -12,10 +11,10 @@ var dataon=new Array();
 var dataoff=new Array();
 var online_Clinets_data=new Array();
 var offline_Clinets_data=new Array();
+var i=0;
 $(function(){
 
-        getData(); //同步一次性取全部数据
-        allClient();//首页展示所有客户
+    getData(); //同步一次性取全部数据
 
 
     //点击“查询单个客户”时才出现输入框
@@ -28,23 +27,39 @@ $(function(){
     //查询单个客户
     $('#search').click(function(e){
         e.preventDefault();
+        $('#index_pic').hide();
         $('#picContainer').hide();
         $('#hashContainer').hide();
         $('#tableContainer').show();
+        var clinetId=$('#singleId').val();
+        console.log(clinetId);
+        var url="http://172.16.38.169:8089/client/status/"+clinetId;
+        console.log(url);
         $.ajax(
             {
+                async:true,//使用同步的Ajax请求
                 type:"GET",
-                //url:"http://172.16.38.169:8089/client/statusList",
+                //url:url,
                 url:"data2.json",
                 dataType:"json",
                 success:function(data){
-                    $('ul.pagination').hide();
+                    //如果用户存在
                     var txtHtml="";
-                    if(data.status=="online")
-                    txtHtml+='<tr class="success"><td>'+data['clientId']+'</td><td>'+data['status']+'</td><td>'+data['timestamp']+'</td></tr>';
-                    else
-                        txtHtml+='<tr><td>'+data['clientId']+'</td><td>'+data['status']+'</td><td>'+data['timestamp']+'</td></tr>';
-                    $('#table').html(txtHtml);
+                    if(data['clientId']!=null){
+                        $('ul.pagination').hide();
+
+                        if(data.status=="online")
+                            txtHtml+='<tr class="success"><td>'+data['clientId']+'</td><td>'+data['status']+'</td><td>'+data['timestamp']+'</td></tr>';
+                        else
+                            txtHtml+='<tr><td>'+data['clientId']+'</td><td>'+data['status']+'</td><td>'+data['timestamp']+'</td></tr>';
+                        $('#table').html(txtHtml);
+                    }
+                    //如果用户不存在
+                    else{
+                        $('#tableContainer').hide();
+                        txtHtml='<p style="text-align: center;margin: 0.7em 0 1em 0;font-size: 2em;background:steelblue;color:#ffffff;" >您查找的用户不存在</p>';
+                        $('#content').html(txtHtml);
+                    }
                 }
             }
         )
@@ -57,6 +72,7 @@ $(function(){
     //上线、下线客户数目饼状图
    $('#pie').click(function(e){
        e.preventDefault();
+       $('#index_pic').hide();
        $('#tableContainer').hide();
        $('#picContainer').show();
        $('#hashContainer').hide();
@@ -64,13 +80,59 @@ $(function(){
            chart: {
                plotBackgroundColor: null,
                plotBorderWidth: null,
-               plotShadow: false
+               plotShadow: false,
+               events: {
+                   load: function () {
+                       // 图表每秒更新一次
+                       var series = this.series[0];
+                       setInterval(function () {
+                           $.ajax(
+                               {
+                                   async:true,//使用同步的Ajax请求
+                                   type:"GET",
+                                   url:"data.json",
+                                   //url:url,
+                                   dataType:"json",
+                                   success:function(data){
+                                       num=data['total'];
+                                       datalist=data['data'];
+                                       datasort=datalist.sort(cmp);
+                                       onNum=0;
+                                       offNum=0;
+                                       online_Clinets_data=[];
+                                       offline_Clinets_data=[];
+                                       for(var h=0;h<num;h++){
+                                           if(datalist[h]['status']=='online') {
+                                               onNum++;
+                                               online_Clinets_data.push(datalist[h]);
+                                           }
+
+                                           else {
+                                               offNum++;
+                                               offline_Clinets_data.push(datalist[h]);
+                                           }
+                                       }
+                                       var data = [];
+                                       data.push(['online', 100*onNum/num]);
+                                       data.push(['offline', 100*offNum/num]);
+                                       series.setData(data);
+                                       console.log(data);
+
+                                   },
+                                   error:function(XMLHttpRequest, textStatus, errorThrown) {
+                                       console.log(XMLHttpRequest.status);
+                                       console.log(XMLHttpRequest.readyState);
+                                       console.log(textStatus);
+                                   }
+                               }
+                           );
+
+                       }, 1000);
+                   }
+               }
            },
            title: {
                text: '上线、下线客户对比'
-           },
-           subtitle: {
-               text: 'online：'+onNum+' offline:'+offNum
            },
 
            tooltip: {
@@ -101,19 +163,12 @@ $(function(){
                type: 'pie',
               // name: 'online vs offline',
                data: [
-                   {
-                       name: 'offline',
-                       y: offNum/num,
-                       sliced: true,
-                       selected: true,
-                       color:'#EAC7CD'
-
-                   },
-                   {
-                       name:'online',
-                       y:onNum/num
-
-                   }
+                   [
+                       "online", 100*offNum/num
+                   ],
+                   [
+                       "offline",100*onNum/num
+                   ]
 
 
                ]
@@ -128,72 +183,106 @@ $(function(){
         return da1-da2;
     }
 
-    //自定义Date的格式
-    Date.prototype.Format = function (fmt) { //author: meizz
-        var o = {
-            "M+": this.getMonth() + 1, //月份
-            "d+": this.getDate(), //日
-            "h+": this.getHours(), //小时
-            "m+": this.getMinutes(), //分
-            "s+": this.getSeconds(), //秒
-            "q+": Math.floor((this.getMonth() + 3) / 3), //季度
-            "S": this.getMilliseconds() //毫秒
-        };
-        if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
-        for (var k in o)
-            if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
-        return fmt;
-    }
-
 
     //online,offline时间分布.日期为横坐标，小时分钟为纵坐标
     $('#hash').click(function(e) {
+        getData();//获得数据
         e.preventDefault();
+        $('#index_pic').hide();
         $('#tableContainer').hide();
-
         $('#picContainer').hide();
         $('#hashContainer').show();
         var str = new Array(num);
-
-        var ony = new Array(num);
-        var offy = new Array(num);
-
-        for (var ii = 0; ii < num - 1; ii++) {
-
+        dataon=[];
+        dataoff=[];
+        for (var ii = 0; ii < num; ii++) {
             var temp = datasort[ii]['timestamp'].split(' ');
             var yy = parseInt(temp[1].substr(0, 2)) * 3600 + parseInt(temp[1].substr(3, 2)) * 60 + parseInt(temp[1].substr(6, 2));
             yy = yy * 1000;
-            //var yy=new Date(datasort[ii]['timestamp']).Format("hh:mm");
-            //var d = new Date(datasort[ii]['timestamp']).Format("yyyy-MM-dd");
             var arr=temp[0].split('-');
             var d=Date.UTC(arr[0],arr[1]-1,arr[2]);
-            //str+= d.getFullYear()+'/'+ (d.getMonth()+1)+'/'+ d.getDate()+',';
-            //var d=new Date(temp[0]);
-
-            str.push(d);
+            //str.push(d);
+            console.log("d="+d);
+            console.log("yy="+yy);
+            console.log("behind");
             if (datasort[ii]['status'] == 'online') {
                 dataon.push([d,yy]);
-                ony.push(yy);
+
             }
             else {
                 dataoff.push([d,yy]);
-                offy.push(yy);
+
             }
         }
 
-        str.push(temp[0]);
-        if (datasort[ii]['status'] == 'online') {
-            ony.push(yy);
-        }
-        else {
-            offy.push(yy);
-        }
+
 
 
         $('#hashContainer').highcharts({
             chart: {
                 type: 'scatter',
-                zoomType: 'xy'
+                zoomType: 'xy',
+                events: {
+                    load: function () {
+                        // 图表每秒更新一次
+                        var series = this.series[0];
+                        dataon=[];
+                        dataoff=[];
+                        setInterval(function () {
+                            $.ajax(
+                                {
+                                    async:true,//使用同步的Ajax请求
+                                    type:"GET",
+                                    url:"data.json",
+                                    //url:url,
+                                    dataType:"json",
+                                    success:function(data){
+                                        num=data["total"];
+                                        var str = new Array(num);
+                                        dataon=[];//清空
+                                        dataoff=[];//清空
+                                        for (var ii = 0; ii < num; ii++) {
+                                            var temp = datasort[ii]['timestamp'].split(' ');
+                                            var yy = parseInt(temp[1].substr(0, 2)) * 3600 + parseInt(temp[1].substr(3, 2)) * 60 + parseInt(temp[1].substr(6, 2));
+                                            yy = yy * 1000;
+                                            var arr=temp[0].split('-');
+                                            var d=Date.UTC(arr[0],arr[1]-1,arr[2]);
+                                            //str.push(d);
+                                            if (datasort[ii]['status'] == 'online') {
+                                                dataon.push([d,yy]);
+
+                                            }
+                                            else {
+                                                dataoff.push([d,yy]);
+
+                                            }
+                                            console.log("d="+d);
+                                            console.log("yy="+yy);
+                                        }
+                                        var data=[];
+                                        var online={};
+                                        var offline=[];
+                                        online.name="online";
+                                        online.color="rgba(223, 83, 83, .5)";
+                                        online.data=dataon;
+                                        offline.name="offline";
+                                        offline.color="rgba(119, 152, 191, .5)";
+                                        offline.data=dataoff;
+                                        data.push(online);
+                                        data.push(offline);
+
+                                    },
+                                    error:function(XMLHttpRequest, textStatus, errorThrown) {
+                                        console.log(XMLHttpRequest.status);
+                                        console.log(XMLHttpRequest.readyState);
+                                        console.log(textStatus);
+                                    }
+                                }
+                            );
+
+                        }, 1000);
+                    }
+                }
             },
             title: {
                 text: '上线/下线时间分布'
@@ -206,11 +295,12 @@ $(function(){
                 //categories: str,
                 title: {
                     text: '日期'
-                },
-
-                dateTimeLabelFormats: {
-                    millisecond: '%Y:%m:%d'
                 }
+                //,
+                //
+                //dateTimeLabelFormats: {
+                //    millisecond: '%Y:%m:%d'
+                //}
                // pointInterval: 24 * 3600 * 1000
 
             },
@@ -218,10 +308,10 @@ $(function(){
                 title: {
                     text: '时间'
                 },
-                type: 'datetime',
-                dateTimeLabelFormats: {
-                    millisecond: '%H:%M:%S.%L'
-                }
+                type: 'datetime'
+                //dateTimeLabelFormats: {
+                //    millisecond: '%H:%M:%S.%L'
+                //}
 
             },
             legend: {
@@ -286,11 +376,13 @@ $(function(){
 
     //查询在线用户
     $('#online_clients').click(function(e){
+        getData();//获得数据
         e.preventDefault();
         onlineClient();
     });
     //查询离线用户
     $('#offline_clients').click(function(e){
+        getData();//获得数据
         e.preventDefault();
         offlineClient();
     });
@@ -298,17 +390,22 @@ $(function(){
 
     //一次性取全部数据，因为后面都是用这同一个数据就不异步了
     function getData(){
+        var url="http://172.16.38.169:8089/client/statusList";
         $.ajax(
             {
-                async:false,//使用同步的Ajax请求
+                async:true,//使用同步的Ajax请求
                 type:"GET",
-                url:"data.json",
+               url:"data.json",
+                //url:url,
                 dataType:"json",
                 success:function(data){
                     num=data['total'];
                     datalist=data['data'];
                     datasort=datalist.sort(cmp);
-
+                    onNum=0;
+                    offNum=0;
+                    online_Clinets_data=[];
+                    offline_Clinets_data=[];
                     for(var h=0;h<num;h++){
                         if(datalist[h]['status']=='online') {
                             onNum++;
@@ -323,18 +420,20 @@ $(function(){
 
                 },
                 error:function(XMLHttpRequest, textStatus, errorThrown) {
-                    alert(XMLHttpRequest.status);
-                    alert(XMLHttpRequest.readyState);
-                    alert(textStatus);
+                    console.log(XMLHttpRequest.status);
+                    console.log(XMLHttpRequest.readyState);
+                    console.log(textStatus);
                 }
             }
         );
         //setTimeout('getData()',4000);
-
+        console.log("getData()");
     }
 
     //展示所有客户
     function allClient(){
+        getData();//获得数据
+        $('#index_pic').hide();
         $('#singleForm').hide();
         $('#picContainer').hide();
         $('#hashContainer').hide();
@@ -375,6 +474,8 @@ $(function(){
 
     //展示在线用户
     function onlineClient(){
+        getData();//获得数据
+        $('#index_pic').hide();
         $('#singleForm').hide();
         $('#picContainer').hide();
         $('#hashContainer').hide();
@@ -392,7 +493,8 @@ $(function(){
 
     //展示离线用户
     function offlineClient(){
-
+        getData();//获得数据
+        $('#index_pic').hide();
         $('#singleForm').hide();
         $('#picContainer').hide();
         $('#hashContainer').hide();
